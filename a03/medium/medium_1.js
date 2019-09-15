7ff39527422ce065cebc7c260e262096d5f8fc7b
William Rogers <williamsrogers98@gmail.com>
2019-09-15 18:10:24 -0400

    Add files via upload

---
+++ a03/medium/medium_1.js
@@ -0,0 +1,71 @@
+import {variance} from "./data/stats_helpers";
+
+
+/**
+ * Gets the sum of an array of numbers.
+ * @param array
+ * @returns {*}
+ * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ * prototype functions. Very useful
+ */
+export function getSum(array) {
+    var sum=0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i]
+      }
+      return sum;
+}
+
+
+/**
+ * Calculates the mean of an array of numbers.
+ * @param {number[]} array
+ * @returns {number|*}
+ *
+ * example:
+ * let array = [3,2,5,6,2,7,4,2,7,5];
+ * console.log(getMedian(array)); // 4.5
+ */
+export function getMedian(array) {
+   var median =0;
+   array.sort((a,b)=> a-b);
+    if( array.length%2 ==0){
+        median =(array[array.length/2-1] + array[array.length/2])/2;
+    }
+    else{
+        median = array[(array.length-1)/2];
+    }
+    return median;
+}
+/**
+ * Calculates statistics (see below) on an array of numbers.
+ * Look at the stats_helper.js file. It does variance which is used to calculate std deviation.
+ * @param {number[]} array
+ * @returns {{min: *, median: *, max: *, variance: *, mean: *, length: *, sum: *, standard_deviation: *}}
+ *
+ * example:
+ * getStatistics([3,2,4,5,5,5,2,6,7])
+ * {
+  length: 9,
+  sum: 39,
+  mean: 4.333333333333333,
+  median: 5,
+  min: 2,
+  max: 7,
+  variance: 2.6666666666666665,
+  standard_deviation: 1.632993161855452
+ }
+ */
+export function getStatistics(array) {
+let obj = {};
+obj.length = array.length;
+obj.sum = getSum(array);
+obj.mean = obj.sum/obj.length;
+obj.median = getMedian(array);
+array.sort();
+obj.min = array[0];
+obj.max = array[array.length-1]
+obj.variance =  variance(array, obj.mean);
+obj.standard_deviation = Math.sqrt(obj.variance);
+return obj;
+}
---
+++ a03/medium/medium_2.js
@@ -0,0 +1,153 @@
+import mpg_data from "./data/mpg_data";
+import {getStatistics} from "./medium_1";
+
+/*
+This section can be done by using the array prototype functions.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+see under the methods section
+*/
+
+
+/**
+ * This object contains data that has to do with every car in the `mpg_data` object.
+ *
+ *
+ * @param {allCarStats.avgMpg} Average miles per gallon on the highway and in the city. keys `city` and `highway`
+ *
+ * @param {allCarStats.allYearStats} The result of calling `getStatistics` from medium_1.js on
+ * the years the cars were made.
+ *
+ * @param {allCarStats.ratioHybrids} ratio of cars that are hybrids
+ */
+export function  getAveragempg(array){
+    var highwaycount =0;
+    var citycount =0;
+    var citysum=0;
+    var highwaysum =0;
+    for (var i = 0; i < array.length; i++) {
+    citysum+= array[i].city_mpg;
+    highwaysum+= array[i].highway_mpg;
+    highwaycount++;
+    citycount++;
+    }
+   let city = citysum/citycount;
+   let highway =highwaysum/highwaycount; 
+    return {city, highway};
+}
+
+export function getallYearStats(array){
+ let arr = []
+ for (var i = 0; i < array.length; i++) {
+     arr[i]= mpg_data[i].year;
+ }
+ return getStatistics(arr);
+}
+export function getHybridRatio(array){
+var hybridcount =0;
+var total =0;
+var count =0;
+for (var i = 0; i < array.length; i++) {
+    count++;
+    if( mpg_data[i].hybrid){
+        hybridcount++
+    }
+}
+    return hybridcount/count;
+}
+export const allCarStats = {
+    avgMpg: getAveragempg(mpg_data),
+    allYearStats: getallYearStats(mpg_data),
+    ratioHybrids: getHybridRatio(mpg_data),
+    
+        
+};
+
+/**
+ * HINT: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+ *
+ * @param {moreStats.makerHybrids} Array of objects where keys are the `make` of the car and
+ * a list of `hybrids` available (their `id` string). Don't show car makes with 0 hybrids. Sort by the number of hybrids
+ * in descending order.
+ *
+ *[{
+ *     "make": "Buick",
+ *     "hybrids": [
+ *       "2012 Buick Lacrosse Convenience Group",
+ *       "2012 Buick Lacrosse Leather Group",
+ *       "2012 Buick Lacrosse Premium I Group",
+ *       "2012 Buick Lacrosse"
+ *     ]
+ *   },
+ *{
+ *     "make": "BMW",
+ *     "hybrids": [
+ *       "2011 BMW ActiveHybrid 750i Sedan",
+ *       "2011 BMW ActiveHybrid 750Li Sedan"
+ *     ]
+ *}]
+ *
+ *
+ *
+ *
+ * @param {moreStats.avgMpgByYearAndHybrid} Object where keys are years and each year
+ * an object with keys for `hybrid` and `notHybrid`. The hybrid and notHybrid
+ * should be an object with keys for `highway` and `city` average mpg.
+ *
+ * Only years in the data should be keys.
+ *
+ * {
+ *     2020: {
+ *         hybrid: {
+ *             city: average city mpg,
+ *             highway: average highway mpg
+ *         },
+ *         notHybrid: {
+ *             city: average city mpg,
+ *             highway: average highway mpg
+ *         }
+ *     },
+ *     2021: {
+ *         hybrid: {
+ *             city: average city mpg,
+ *             highway: average highway mpg
+ *         },
+ *         notHybrid: {
+ *             city: average city mpg,
+ *             highway: average highway mpg
+ *         }
+ *     },
+ *
+ * }
+ */
+
+export function getMakerHybrids(){
+    var result = mpg_data.reduce(function(acc, value){
+        if (acc.findIndex(elem => elem.make === value.make)==-1){
+            let hybrids = mpg_data.filter(item => item.make === value.make).filter(item => item.hybrid).map(item => item.id)
+            if (hybrids.length >0){
+                acc.push({make: value.make, hybrids});
+            }
+            }
+            return acc;
+        },[]);
+        return result;
+}
+export function getMpgbyYearAndHybrid(){
+    let year= mpg_data.map(elem => elem.year);
+    let object = {};
+    let hybrid =mpg_data.filter(elem => elem.hybrid === true);
+    let reg =mpg_data.filter( elem => elem.hybrid ===false);
+    for(let i=0; i<year.length; i++){
+        let years = year[i];
+        let obj ={hybrid: getAveragempg(hybrid.filter(elem => elem.year == years)),
+            notHybrid: getAveragempg(reg.filter(elem => elem.year ==years))
+        }
+            object[years] =obj;
+
+    };
+    return object;
+}
+export const moreStats = {
+    makerHybrids: getMakerHybrids(),
+    avgMpgByYearAndHybrid: getMpgbyYearAndHybrid()
+};
